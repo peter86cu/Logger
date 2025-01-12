e851ad2a29ab3a0420a7d93ddd06bf04aee3d6df
peter86cu <39494828+peter86cu@users.noreply.github.com>
2025-01-12 18:11:13 -0300

    Add @RequestMapping("/logger")

--- src/main/java/controller/LoggerController.java
+++ src/main/java/controller/LoggerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,6 +23,7 @@ import service.LoggerService;
 
 
 @RestController
+@RequestMapping("/logger")
 public class LoggerController {
 
 	@Autowired
